67f39cfb44a8001613bd8cd3d9041e82d624da61
Xavier Gimenez Sanchez <xgimenez@enplater.local>
2016-08-23 12:03:21 +0200

    added dist at gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var uglify = require("gulp-uglify");
 var ngAnnotate = require("gulp-ng-annotate");
 var sourcemaps = require("gulp-sourcemaps");
 var nodemon = require("gulp-nodemon");
+var clean = require('gulp-clean');
+var runSequence = require('run-sequence');
+
 
 gulp.task('dev:server', function() {
     nodemon({
@@ -36,23 +39,46 @@ gulp.task('watch:js', ['js-dev'], function() {
 });
 
 
-gulp.task('copy-dist', function() {
-    gulp.src(['assets/**/*.*'])
-        .pipe(gulp.dest("dist/assets"));
-})
-gulp.task('js-dist', function () {
-    
+gulp.task('del-dist', function() {
+    return gulp.src('dist')
+        .pipe(clean());
+});
 
-    gulp.src(['ng/module.js','ng/**/*.js'])
+gulp.task('copy-dist', ['del-dist'], function() {
+    return gulp.src(['assets/**/*.*'])
+        .pipe(gulp.dest("dist/assets"));
+    /*gulp.src('dist/assets/js/vilobiApp.js')
+        .pipe(clean({force:true}));*/
+});
+gulp.task('js-dist', ['copy-dist'], function () {
+    return gulp.src(['ng/module.js','ng/**/*.js'])
         .pipe(sourcemaps.init())
             .pipe(concat('vilobiApp.js'))
             .pipe(ngAnnotate())
             .pipe(uglify())
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest("dist/assets/js"));
+        .pipe(gulp.dest("dist/assets/js")); 
 });
 
 
+gulp.task('controllers-dist',['js-dist'],function() {
+    return gulp.src(['controllers/**/*.js'])
+                .pipe(gulp.dest('dist/controllers'))
+});
+
+gulp.task('layouts-dist',['controllers-dist'],function() {
+    return gulp.src(['layouts/app.html','layouts/machine.html'])
+                .pipe(gulp.dest('dist/layouts'));
+});
+
+gulp.task('templates-dist',['layouts-dist'], function() {
+    return gulp.src(['templates/**/*.html'])
+                .pipe(gulp.dest('dist/templates'));
+})
+
 gulp.task('dev', ['watch:js', 'dev:server']);
 
-gulp.task('dist', ['copy-dist','js-dist']);
+gulp.task('dist',['templates-dist'], function() {
+    return gulp.src(['auth.js','config.js','db.js','server.js'])
+                .pipe(gulp.dest('dist'));
+});
